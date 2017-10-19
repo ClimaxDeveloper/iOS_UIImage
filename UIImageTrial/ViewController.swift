687ac44bdc37e82e3953ce8d817a605323c67230
Max Li <maxli@Maxs-MacBook-Pro.local>
2017-10-19 15:26:39 -0700

    Push project onto Github

---
+++ UIImageTrial/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  UIImageTrial
+//
+//  Created by Max Li on 10/14/17.
+//  Copyright © 2017 ClimaxDeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func pickAnImage(_ sender: Any) {
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
